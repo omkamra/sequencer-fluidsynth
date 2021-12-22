fed9e2a1e20ae18f9c02742d18949e1039785ac6
Ruzsa Balazs <ruzsa.balazs@gmail.com>
2021-12-22 14:51:26 +0100

    Version 0.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Change Log
 
+## [0.2.1] - 2021-12-22
+
+### Changed
+
+- bump `omkamra.sequencer-midi` dependency to `0.2.0`
+
 ## [0.2.0] - 2021-12-22
 
 ### Changed
@@ -10,5 +16,6 @@
 
 Initial release.
 
+[0.2.1]: https://github.com/omkamra/sequencer-fluidsynth/compare/0.2.0...0.2.1
 [0.2.0]: https://github.com/omkamra/sequencer-fluidsynth/compare/0.1.0...0.2.0
 [0.1.0]: https://github.com/omkamra/sequencer-fluidsynth/tree/0.1.0
--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject com.github.omkamra/sequencer-fluidsynth "0.2.0"
+(defproject com.github.omkamra/sequencer-fluidsynth "0.2.1"
   :description "An omkamra.sequencer target for the FluidSynth SoundFont synthesizer"
   :url "https://github.com/omkamra/sequencer-fluidsynth"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [com.github.omkamra/fluidsynth "0.2.0"]
-                 [com.github.omkamra/sequencer-midi "0.1.0"]])
+                 [com.github.omkamra/sequencer-midi "0.2.0"]])
